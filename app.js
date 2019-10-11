c78bdd9e9d1d2aefbb866cd5c2c24b49ba6d26d8
trungdp <trungdp.labianlabs@gmail.com>
2019-10-11 18:21:14 +0700

    ejs

--- app.js
+++ app.js
@@ -11,7 +11,19 @@ app.set("views", "./views");
 
 //Tạo socket 
 app.get("/", function(req, res) {
-    res.render("index");
+    var posts = [{title:"Man must explore, and this is exploration at its greatest",
+                 subtitle:"Problems look mighty small from 150 miles up",
+                 author:"Start Bootstrap"},
+                 {title:"I believe every human has a finite number of heartbeats. I don't intend to waste any of mine.",
+                 subtitle:"Problems look mighty small from 150 miles up",
+                 author:"Start Bootstrap"},
+                 {title:"Science has not yet mastered prophecy",
+                 subtitle:"We predict too much for the next year and yet far too little for the next ten.",
+                 author:"Start Bootstrap"},
+                 {title:"Failure is not an option",
+                 subtitle:"Many say exploration is part of our destiny, but it’s actually our duty to future generations.",
+                 author:"Start Bootstrap"}]
+    res.render("index",{posts:posts});
 });
 
 server.listen(process.env.PORT || 8000);
